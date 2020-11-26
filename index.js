8537dd6f68aa2c197596b62eee7f30c31210421f
dlutrick <davidrlutrick@gmail.com>
2020-11-25 23:26:02 -0700

    added dropdown menu highlighting

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Dropdown Menu Functionality
-
 const toggleMenu = () => {
   let menu = document.getElementById("menu");
   menu.classList.toggle("show_menu");
 };
 
+// Toggle Dropdown Menu Icon
 const classToggle = () => {
   document.getElementById("toggle_button").classList.toggle("fa-bars");
   document.getElementById("toggle_button").classList.toggle("fa-times");
